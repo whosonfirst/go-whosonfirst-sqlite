8d967fdb681827aabcb448fdd64c7f32f518e5d7
thisisaaronland <whosonfirst@mapzen.com>
2017-08-25 22:58:57 +0000

    start implementing the spr table

--- tables/spr.go
+++ tables/spr.go
@@ -2,7 +2,7 @@ package tables
 
 import (
 	"errors"
-	_ "fmt"
+	"fmt"
 	"github.com/whosonfirst/go-whosonfirst-geojson-v2"
 	"github.com/whosonfirst/go-whosonfirst-sqlite"
 )
@@ -10,6 +10,18 @@ import (
 type SPRTable struct {
 	sqlite.Table
 	name string
+}
+
+type SPRRow struct {
+     Id     int64		// properties.wof:id	INTEGER
+     Name   string		// properties.wof:name  VARCHAR(255)
+     Placetype	string		// properties.wof:placetype VARCHAR(64)
+     Country	string		// properties.wof:country VARCHAR(2)
+     IsCurrent	int64
+     IsCeased	int64
+     IsDeprecated int64
+     IsSuperseded int64
+     IsSuperseding int64
 }
 
 func NewSPRTable() (*SPRTable, error) {
@@ -30,7 +42,20 @@ func (t *SPRTable) Name() string {
 }
 
 func (t *SPRTable) Schema() string {
-	return ""
+
+	sql := `CREATE TABLE %s (
+			id INTEGER NOT NULL PRIMARY KEY,
+			name TEXT,
+			placetype TEXT,
+			country TEXT,
+			is_current INTEGER,
+			is_deprecated INTEGER,
+			is_ceased INTEGER,
+			is_superseded INTEGER,
+			is_superseding INTEGER
+	)`
+
+	return fmt.Sprintf(sql, t.Name())
 }
 
 func (t *SPRTable) IndexFeature(db sqlite.Database, f geojson.Feature) error {
