ac0c1e608d9b2912a0a91d3345fc34397d2d7896
thisisaaronland <whosonfirst@mapzen.com>
2017-12-20 15:36:47 +0000

    stub out compress and hashing functions

--- cmd/wof-sqlite-inventory.go
+++ cmd/wof-sqlite-inventory.go
@@ -1,22 +1,143 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
 	"flag"
+	"fmt"
 	"github.com/whosonfirst/go-whosonfirst-sqlite/database"
+	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 	"time"
 )
 
 type Report struct {
-	Path         string
-	Include      bool
-	Count        int
-	Size         int64
-	LastUpdate   time.Time
-	LastModified time.Time
+	Path           string
+	Include        bool
+	Count          int
+	Size           int64
+	SizeCompressed int64
+	Sha1Sum        string
+	LastUpdate     time.Time
+	LastModified   time.Time
+}
+
+func NewReport(path string) Report {
+
+	now := time.Now()
+
+	r := Report{
+		Path:           path,
+		Include:        false,
+		Count:          0,
+		Size:           0,
+		SizeCompressed: 0,
+		Sha1Sum:        "",
+		LastModified:   now,
+		LastUpdate:     now,
+	}
+
+	return r
+}
+
+func Compress(r Report, report_ch chan Report, done_ch chan bool, err_ch chan error) {
+
+	defer func() {
+		done_ch <- true
+	}()
+
+	abs_source, err := filepath.Abs(r.Path)
+
+	if err != nil {
+		err_ch <- err
+		return
+	}
+
+	abs_chroot := filepath.Dir(abs_source)
+
+	fname := filepath.Base(abs_source)
+	fname = fmt.Sprintf("%s.bz2", fname)
+
+	dest := filepath.Join(abs_chroot, fname)
+
+	info, err := os.Stat(dest)
+
+	if os.IsNotExist(err) {
+
+		bz := "bzip2"
+
+		args := []string{
+			"-f",
+			"-k", // Keep (don't delete) input files during compression or decompression.
+			abs_source,
+		}
+
+		log.Println(bz, strings.Join(args, " "))
+
+		cmd := exec.Command(bz, args...)
+		err = cmd.Run()
+
+		if err != nil {
+			err_ch <- err
+			return
+		}
+
+		info, err = os.Stat(dest)
+
+		if err != nil {
+			err_ch <- err
+			return
+		}
+
+	} else {
+
+		if err != nil {
+			err_ch <- err
+			return
+		}
+
+	}
+
+	r.SizeCompressed = info.Size()
+	report_ch <- r
+	return
+}
+
+func Hash(r Report, report_ch chan Report, done_ch chan bool, err_ch chan error) {
+
+	defer func() {
+		done_ch <- true
+	}()
+
+	path := fmt.Sprintf("%s.bz2", r.Path)
+
+	fh, err := os.Open(path)
+
+	if err != nil {
+		err_ch <- err
+		return
+	}
+
+	body, err := ioutil.ReadAll(fh)
+
+	if err != nil {
+		err_ch <- err
+		return
+	}
+
+	defer fh.Close()
+
+	hash := sha1.Sum(body)
+	enc := hex.EncodeToString(hash[:])
+
+	r.Sha1Sum = enc
+
+	report_ch <- r
+	return
 }
 
 func Inventory(path string, report_ch chan Report, done_ch chan bool, error_ch chan error) {
@@ -56,16 +177,7 @@ func Inventory(path string, report_ch chan Report, done_ch chan bool, error_ch c
 		return
 	}
 
-	now := time.Now()
-
-	r := Report{
-		Path:         abs_path,
-		Include:      false,
-		Count:        0,
-		Size:         0,
-		LastModified: now,
-		LastUpdate:   now,
-	}
+	r := NewReport(abs_path)
 
 	if count == 0 {
 		report_ch <- r
@@ -116,7 +228,7 @@ func main() {
 
 	remaining := count
 
-	// reports := make(map[string]Report)
+	reports := make([]Report, 0)
 
 	for remaining > 0 {
 
@@ -128,12 +240,46 @@ func main() {
 		case r := <-report_ch:
 
 			if r.Include {
-
-				// do stuff here...
+				reports = append(reports, r)
 			} else {
 				log.Println("REMOVE", r.Path)
 				os.Remove(r.Path)
 			}
+		}
+	}
+
+	/*
+		count_throttle := 8
+		throttle_ch := make(chan bool, count_throttle)
+
+		for i :=0; i < count_throttle; i ++ {
+			throttle_ch <- true
+		}
+	*/
+
+	for _, r := range reports {
+
+		log.Println("WAITING FOR THROTTLE")
+
+		// throttle_ch
+
+		go func(r Report) {
+			Compress(r, report_ch, done_ch, error_ch)
+		}(r)
+	}
+
+	remaining = len(reports)
+
+	for remaining > 0 {
+
+		select {
+		case <-done_ch:
+			log.Println("DONE", remaining)
+			remaining -= 1
+		case err := <-error_ch:
+			log.Println("ERROR", err)
+		case r := <-report_ch:
+			log.Println("REPORT", r)
 		}
 	}
 
